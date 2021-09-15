1dc8a09b340e73e5c67642650b6dff20ed2b6fc5
Angelica Cortez <angelica.cortez@hotmail.it>
2021-09-15 11:58:20 +0200

    Inserimento localstorage su login

--- ScriptHotel.js
+++ ScriptHotel.js
@@ -10,3 +10,44 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+
+function save() {
+    let insertedUsername = document.getElementById("username").value;
+    let insertedPassword = document.getElementById("password").value;
+    localStorage.setItem('username', insertedUsername);
+    localStorage.setItem('password', insertedPassword);
+
+}
+
+
+function hideLogin() {
+
+    document.getElementById("loginformbtn").style.display = "none";
+    document.getElementById("logoutbtn").style.display = "";
+}
+
+function logoutForm() {
+    let storedUsername = localStorage.getItem('username');
+    let storedPassword = localStorage.getItem('password');
+    if (storedUsername != null && storedPassword != null)
+    {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+        let LogBtn = document.getElementById("loginformbtn");
+        LogBtn.style.display = "";
+        let LogoutBtn = document.getElementById("logoutbtn");
+        LogoutBtn.style.display = "none";
+        alert("Logout avvenuto con successo");
+    } else
+    {
+        alert("Non trovato");
+    }
+
+}
+
+
+
+
+
+
